3dc6110594ca41519a179c47da3c599dae0d1a15
LamLe <vulam1808@gmail.com>
2017-08-16 20:09:43 +0700

    no message

--- application/models/InfoBusiness.php
+++ application/models/InfoBusiness.php
@@ -113,6 +113,54 @@ class Model_InfoBusinessMapper extends Model_InfoBusinessMapperBase{
         }
         return $entries;
     }
+    public function fetchAllNotByUser($type_business,$userId){
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $select="select * from info_business where type_business='".$type_business."' and created_by != '".$userId."'";
+        $stmt=$db->query($select);
+        $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
+        $stmt->closeCursor();
+        $entries = array();
+        foreach ($rows as $row){
+            $entry = new Model_InfoBusiness();
+            $entry->setId($row->id)
+                ->setId($row->id)
+                ->setCode($row->code)
+                ->setName($row->name)
+                ->setLicense_Business($row->license_business)
+                ->setDate_License($row->date_license)
+                ->setPlace_License($row->place_license)
+                ->setAddress_Office($row->address_office)
+                ->setAddress_Office2($row->address_office2)
+                ->setAddress_Branch($row->address_branch)
+                ->setAddress_Produce($row->address_produce)
+                ->setWork_Business($row->work_business)
+                ->setPhone($row->phone)
+                ->setBoss_Business($row->boss_business)
+                ->setAddress_Permanent($row->address_permanent)
+                ->setCellphone($row->cellphone)
+                ->setLicense_Condition_Business($row->license_condition_business)
+                ->setDate_License_Condition_Business($row->date_license_condition_business)
+                ->setMaster_Items_Limit_Id($row->master_items_limit_id)
+                ->setMaster_Items_Condition_Id($row->master_items_condition_id)
+                ->setMaster_Province_Id($row->master_province_id)
+                ->setMaster_District_Id($row->master_district_id)
+                ->setMaster_Ward_Id($row->master_ward_id)
+                ->setMaster_Business_Type_Id($row->master_business_type_id)
+                ->setMaster_Business_Size_Id($row->master_business_size_id)
+                ->setDate_Check($row->date_check)
+                ->setType_Business($row->type_business)
+                ->setCreated_date($row->created_date)
+                ->setCreated_By($row->created_by)
+                ->setModified_date($row->modified_date)
+                ->setModified_By($row->modified_by)
+                ->setOrder($row->order)
+                ->setStatus($row->status)
+                ->setIs_delete($row->is_delete)
+                ->setComment($row->comment);
+            $entries[]=$entry;
+        }
+        return $entries;
+    }
      public function  fetchAll_REPORT($wherer){
         $db = Zend_Db_Table::getDefaultAdapter();      
        $select=" select * from info_business ".$wherer."";        
